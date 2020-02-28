41ffa090d26291d185edf186eafdc8f5869cb851
Thibault Trembleau <trembleau.thibault@gmail.com>
2020-02-28 23:42:33 +0100

    ajout test c#

---
+++ POCCSharp/POCCSharp/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCCSharp
+{
+    class User
+    {
+        private string fname; //Prénom
+        private string lname; //Nom de famille
+        private string pw; //Mot de passe
+
+        public User(string fn, string ln, string pw)
+        {
+            fname = fn;
+            lname = pw;
+            this.pw = pw;
+        }
+
+        private void setFirstName(string fn)
+        {
+            fname = fn;
+        }
+        private string getFirstName()
+        {
+            return fname;
+        }
+
+        private void setLastName(string ln)
+        {
+            lname = ln;
+        }
+        private string getLastName()
+        {
+            return lname;
+        }
+
+        private void setPassword(string pw)
+        {
+            this.pw = pw;
+        }
+    }
+}
