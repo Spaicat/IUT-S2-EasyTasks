0250c8a2f6ca342225c946a943181d74e414cb28
Itsiami <57441475+Itsiami@users.noreply.github.com>
2020-03-04 18:30:58 +0100

    Add files via upload

---
+++ ProjetJava/src/projetjava/ProjetJava.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author itsia
+ */
+public class ProjetJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","root");
+            System.out.println("Connecté à la base de données");
+            
+        }catch(Exception e){
+            System.out.println(e);    
+        }
+    }
+    
+}
