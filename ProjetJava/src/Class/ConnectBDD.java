1fd3edb921c4de2495853d60900806289b302935
Itsiami <57441475+Itsiami@users.noreply.github.com>
2020-03-05 17:38:40 +0100

    Add files via upload

--- ProjetJava/src/Class/ConnectBDD.java
+++ ProjetJava/src/Class/ConnectBDD.java
@@ -3,38 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projetjava;
+package Class;
 
-import Class.ConnectBDD;
-import Class.ConnectPage;
-import com.mysql.cj.xdevapi.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-
 
 /**
  *
  * @author itsia
  */
-public class ProjetJava {
+public class ConnectBDD {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        /*
+    private Connection conn = null;
+    
+    public static Connection ConnectBDD(){
+        
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","root");
             System.out.println("Connecté à la base de données");
-            
+            return conn;
+ 
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println(e);   
+            return null;
         }
-        */
-
-        ConnectPage ConnPage = new ConnectPage();
     }
-    
 }
